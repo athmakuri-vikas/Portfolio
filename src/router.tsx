01686da747a6d0888d64405a4d9f0a98df60d9aa
Mahesh0002 <maheshganji057@gmail.com>
2026-07-08 15:29:51 +0530

    Use hash routing for GitHub pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,15 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, createHashHistory } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+
+const hashHistory = createHashHistory();
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
+    history: hashHistory,
     context: { queryClient },
     basepath: import.meta.env.BASE_URL.replace(/\/$/, "") || "/",
     scrollRestoration: true,
